61579c4242671dde9519a598b896b74c42e5b83b
DianaB177 <anaidb77@gmail.com>
2023-03-16 21:11:06 +0100

    navigation building

---
+++ compramiarmario/resources/views/livewire/navigation.blade.php
@@ -0,0 +1,10 @@
+<header class="bg-red-300 ">
+   <div class="container flex items-center h-16">
+    <a href="" class="text-white cursor-pointer">
+        <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+            <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+            <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+        </svg>
+    </a>
+   </div>
+</header>
